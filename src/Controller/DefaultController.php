eb9dfc2e283f00ca0e63e4dc4516596692a18d8c
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-04-06 17:57:04 +0200

    finish challenge 2.5

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController extends AbstractController{
 
-    
+class DefaultController extends AbstractController
+{
+
+
     /**
      * @Route("/", name="app_index")
      */
 
-function index(): Response
-{
-    return $this->render('index.html.twig');
-}
+    public function index(): Response
+    {
 
+        return $this->render('index.html.twig');
+    }
 }
